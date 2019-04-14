c621be22cd085dfb659df90f004c72b2424f75b5
Khaled Saad <elgendy1204@gmail.com>
2019-04-14 10:10:44 +0200

    Add new possible solution

--- server.js
+++ server.js
@@ -107,3 +107,68 @@ client.on('end', () => {
     console.log('disconnected from server');
 });
 */
+
+/* solution 3
+const net = require('net');
+
+const server = net.createServer({ allowHalfOpen: true }, function(clientSock) {
+    let connected = false;
+
+    let serverSock;
+
+    clientSock.on('data', function(clientData) {
+        console.log(clientData);
+        let isTLSConnection = clientData.toString().indexOf('CONNECT') !== -1;
+
+        //Considering Port as 80 by default
+        let serverPort = 80;
+        let serverAddress;
+        if (isTLSConnection) {
+            // Port changed to 443, parsing the host from CONNECT
+            serverPort = 443;
+            serverAddress = clientData
+                .toString()
+                .split('CONNECT ')[1]
+                .split(' ')[0]
+                .split(':')[0];
+        } else {
+            // Parsing HOST from HTTP
+            serverAddress = clientData
+                .toString()
+                .split('Host: ')[1]
+                .split('\r\n')[0];
+        }
+
+        if (connected) {
+            // Send future messages if is connected
+            serverSocet.write(clientData);
+        } else {
+            const host = serverAddress;
+            const port = serverPort;
+
+            // if (clientData is a CONNECT request) {
+
+            // Create a new socket to server
+            if (!serverSock) {
+                // Option here
+                serverSock = new net.Socket({ allowHalfOpen: true });
+
+                serverSock.connect(port, host, function() {
+                    // Don't need to forward hello message from client
+                    // Connect method automatically sends it for you
+                    //serverSock.write(clientData);
+
+                    connected = true;
+
+                    clientSock.write('HTTP/1.1 200 OK\r\n');
+                });
+
+                serverSock.on('data', function(serverData) {
+                    clientSock.write(serverData);
+                });
+            }
+            // }
+        }
+    });
+}).listen(3000);
+*/
