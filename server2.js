7e7951e731401c6ba2eee781b62ed89deaf9ca60
Khaled Saad <elgendy1204@gmail.com>
2019-04-18 15:24:32 +0200

    Connect to a static http site

--- server2.js
+++ server2.js
@@ -1,29 +1,29 @@
 const net = require('net');
-const server = net.createServer(socket => {
-    // 'connection' listener
-    console.log('client connected');
-    console.log(socket.localAddress);
-    socket.on('end', () => {
-        console.log('client disconnected');
-    });
-    socket.write('hello\r\n');
-    socket.pipe(socket);
-});
+// const server = net.createServer(socket => {
+    // // 'connection' listener
+    // console.log('client connected');
+    // console.log(socket.localAddress);
+    // socket.on('end', () => {
+        // console.log('client disconnected');
+    // });
+    // socket.write('hello\r\n');
+    // socket.pipe(socket);
+// });
 
-server.on('error', err => {
-    throw err;
-});
+// server.on('error', err => {
+    // throw err;
+// });
 
-server.listen(8124, () => {
-    console.log('server bound');
-});
+// server.listen(8124, () => {
+    // console.log('server bound');
+// });
 
 // =================================================== //
 
-const client = net.createConnection({ port: 8124 }, () => {
+const client = net.createConnection({ port: 80, host: 'theplantlist.org' }, () => {
     // 'connect' listener
     console.log('connected to server!');
-    client.write('world!\r\n');
+    client.write(`GET http://theplantlist.org\r\n\r\n`);
 });
 
 client.on('data', data => {
