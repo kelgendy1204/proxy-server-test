19a90a0dd7b49d14a13f517c94dfe150a56ddc20
Khaled Saad <elgendy1204@gmail.com>
2019-04-18 16:02:27 +0200

    Make proxy for http

--- server2.js
+++ server2.js
@@ -1,36 +1,35 @@
 const net = require('net');
-// const server = net.createServer(socket => {
-    // // 'connection' listener
-    // console.log('client connected');
-    // console.log(socket.localAddress);
+const server = net.createServer(socket => {
+    // 'connection' listener
+    const client = net.createConnection({ port: 80, host: 'theplantlist.org' }, () => {
+        // 'connect' listener
+        console.log('connected to server!');
+        // client.write(`GET http://theplantlist.org \r\n\r\n`);
+    });
+
+    // client.on('data', data => {
+        // console.log(data.toString());
+        // socket.write(data);
+        // client.end();
+    // });
+
+    // client.on('end', () => {
+        // socket.end();
+        // console.log('disconnected from server');
+    // });
+
     // socket.on('end', () => {
         // console.log('client disconnected');
     // });
-    // socket.write('hello\r\n');
-    // socket.pipe(socket);
-// });
 
-// server.on('error', err => {
-    // throw err;
-// });
-
-// server.listen(8124, () => {
-    // console.log('server bound');
-// });
-
-// =================================================== //
-
-const client = net.createConnection({ port: 80, host: 'theplantlist.org' }, () => {
-    // 'connect' listener
-    console.log('connected to server!');
-    client.write(`GET http://theplantlist.org \r\n\r\n`);
+    client.pipe(socket);
+    socket.pipe(client);
 });
 
-client.on('data', data => {
-    console.log(data.toString());
-    client.end();
+server.on('error', err => {
+    throw err;
 });
 
-client.on('end', () => {
-    console.log('disconnected from server');
+server.listen(8124, () => {
+    console.log('server bound');
 });
