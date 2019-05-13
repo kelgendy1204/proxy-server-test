15861bb3b32563b348e963917af6a0597198ff77
Khaled Saad <elgendy1204@gmail.com>
2019-05-13 13:46:26 +0200

    Change proxy code

--- server.js
+++ server.js
@@ -1,139 +1,76 @@
-const net = require('net');
-const server = net.createServer();
+var httpProxy = require('http-proxy');
+var http = require('http');
+var url = require('url');
+var net = require('net');
 
-server.on('connection', clientToProxySocket => {
-    console.log('============ Client Connected To Proxy ============');
+var server = http
+    .createServer(function(req, res) {
+        var urlObj = url.parse(req.url);
+        var target = urlObj.protocol + '//' + urlObj.host;
 
-    // We need only the data once, the starting packet
-    clientToProxySocket.once('data', data => {
-        let isTLSConnection = data.toString().indexOf('CONNECT') !== -1;
-        console.log(data.toString());
+        console.log('Proxy HTTP request for:', target);
 
-        //Considering Port as 80 by default
-        let serverPort = 80;
-        let serverAddress;
-        if (isTLSConnection) {
-            // Port changed to 443, parsing the host from CONNECT
-            serverPort = 443;
-            serverAddress = data
-                .toString()
-                .split('CONNECT ')[1]
-                .split(' ')[0]
-                .split(':')[0];
-        } else {
-            // Parsing HOST from HTTP
-            serverAddress = data
-                .toString()
-                .split('Host: ')[1]
-                .split('\r\n')[0];
-        }
-
-        let proxyToServerSocket = net.createConnection(
-            {
-                host: serverAddress,
-                port: serverPort
-            },
-            () => {
-                console.log('============ PROXY TO SERVER SET UP ==============');
-                if (isTLSConnection) {
-                    //Send Back OK to HTTPS CONNECT Request
-                    clientToProxySocket.write('HTTP/2 200 OK\r\n\n');
-                } else {
-                    proxyToServerSocket.write(data);
-                }
-                // Piping the sockets
-                clientToProxySocket.pipe(proxyToServerSocket);
-                proxyToServerSocket.pipe(clientToProxySocket);
-
-                proxyToServerSocket.on('error', err => {
-                    console.log('PROXY TO SERVER ERROR');
-                    console.log(err);
-                });
-            }
-        );
-        clientToProxySocket.on('error', err => {
-            console.log('CLIENT TO PROXY ERROR');
-            console.log(err);
+        var proxy = httpProxy.createProxyServer({});
+        proxy.on('error', function(err, req, res) {
+            console.log('proxy error', err);
+            res.end();
         });
+
+        proxy.web(req, res, { target: target });
+    })
+    .listen(8124); //this is the port your clients will connect to
+
+var regex_hostport = /^([^:]+)(:([0-9]+))?$/;
+
+var getHostPortFromString = function(hostString, defaultPort) {
+    var host = hostString;
+    var port = defaultPort;
+
+    var result = regex_hostport.exec(hostString);
+    if (result != null) {
+        host = result[1];
+        if (result[2] != null) {
+            port = result[3];
+        }
+    }
+
+    return [host, port];
+};
+
+server.addListener('connect', function(req, socket, bodyhead) {
+    var hostPort = getHostPortFromString(req.url, 443);
+    var hostDomain = hostPort[0];
+    var port = parseInt(hostPort[1]);
+    console.log('Proxying HTTPS request for:', hostDomain, port);
+
+    var proxySocket = new net.Socket();
+    proxySocket.connect(port, hostDomain, function() {
+        proxySocket.write(bodyhead);
+        socket.write('HTTP/' + req.httpVersion + ' 200 Connection established\r\n\r\n');
+    });
+
+    proxySocket.on('data', function(chunk) {
+        socket.write(chunk);
+    });
+
+    proxySocket.on('end', function() {
+        socket.end();
+    });
+
+    proxySocket.on('error', function() {
+        socket.write('HTTP/' + req.httpVersion + ' 500 Connection error\r\n\r\n');
+        socket.end();
+    });
+
+    socket.on('data', function(chunk) {
+        proxySocket.write(chunk);
+    });
+
+    socket.on('end', function() {
+        proxySocket.end();
+    });
+
+    socket.on('error', function() {
+        proxySocket.end();
     });
 });
-
-server.on('error', err => {
-    console.log('SERVER ERROR');
-    console.log(err);
-});
-
-server.on('close', () => {
-    console.log('Client Disconnected');
-});
-
-server.listen(9000, () => {
-    console.log('Server runnig at http://localhost:' + 9000);
-});
-
-// =========================================================
-
-/* solution 3
-const net = require('net');
-
-const server = net.createServer({ allowHalfOpen: true }, function(clientSock) {
-    let connected = false;
-
-    let serverSock;
-
-    clientSock.on('data', function(clientData) {
-        console.log(clientData);
-        let isTLSConnection = clientData.toString().indexOf('CONNECT') !== -1;
-
-        //Considering Port as 80 by default
-        let serverPort = 80;
-        let serverAddress;
-        if (isTLSConnection) {
-            // Port changed to 443, parsing the host from CONNECT
-            serverPort = 443;
-            serverAddress = clientData
-                .toString()
-                .split('CONNECT ')[1]
-                .split(' ')[0]
-                .split(':')[0];
-        } else {
-            // Parsing HOST from HTTP
-            serverAddress = clientData
-                .toString()
-                .split('Host: ')[1]
-                .split('\r\n')[0];
-        }
-
-        if (connected) {
-            // Send future messages if is connected
-            serverSocet.write(clientData);
-        } else {
-            const host = serverAddress;
-            const port = serverPort;
-
-            // if (clientData is a CONNECT request) {
-
-            // Create a new socket to server
-            if (!serverSock) {
-                // Option here
-                serverSock = new net.Socket({ allowHalfOpen: true });
-
-                serverSock.connect(port, host, function() {
-                    // Don't need to forward hello message from client
-                    // Connect method automatically sends it for you
-                    //serverSock.write(clientData);
-
-                    connected = true;
-
-                    clientSock.write('HTTP/1.1 200 OK\r\n');
-                });
-
-                serverSock.on('data', function(serverData) {
-                    clientSock.write(serverData);
-                });
-            }
-            // }
-        }
-    });
-}).listen(3000);
-*/
